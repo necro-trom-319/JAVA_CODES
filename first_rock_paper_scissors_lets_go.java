44da1d560609fbbff6b8a6fbfeb03334b0cb3c18
necro-trom-319 <arjavj319@gmail.com>
2026-07-02 14:43:23 +0530

    Add files via upload

--- first_rock_paper_scissors_lets_go.java
+++ first_rock_paper_scissors_lets_go.java
@@ -1,19 +1,32 @@
 package ONLY_FOR_ME;
 import java.util.Scanner;
+import java.util.Random;
 public class first_rock_paper_scissors_lets_go {
     static void main(String[] args) {
 
         System.out.println("THIS WILL BE MY FIRST PERSONAL PROJECT");
         System.out.println("LETS MAKE ROCK PAPER SCISSORS");
         System.out.println("ENTER WHAT U'VE GOT BABY");
+        Random ra = new Random();
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
+        int a = ra.nextInt(3) +1;
+        String b = sc.nextLine();
         switch (a) {
-            case "paper" -> System.out.println("scissors\n" + "U SUCK AT THIS GAME MY GUY ");
-            case "scissors" -> System.out.println(  "rock\n" + "NAH I WON ");
-            case "rock" -> System.out.println("paper\n" + "YOU'LL NEVER LEARN DO U HUH ");
-            default -> System.out.println("U GOOD ?? DO U KNOW HOW TO PLAY THIS GAME");
+            case 1 -> System.out.println("scissors\n" + "U SUCK AT THIS GAME MY GUY ");
+            case 2 -> System.out.println(  "rock\n" + "NAH U LOOSE ");
+            case 3 -> System.out.println("paper\n" + "YOU'LL NEVER LEARN DO U HUH ");
+//            if (b.equals("paper");a=3; ) {
+//                System.out.println("TIE!!!");
+            }
+
         }
+
+//        switch (b) {
+//            case "paper" -> System.out.println("hee hee how about this now ");
+//            case "scissors" -> System.out.println("yup i won");
+//            case "rock" -> System.out.println("now what cat got your thumb");
+//            default -> System.out.println("i_'ll beat u to pulp one day");
+//        }
 
 
 
@@ -22,5 +35,5 @@ public class first_rock_paper_scissors_lets_go {
 
 
     }
-}
+
 
